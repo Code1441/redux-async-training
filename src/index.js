832a62e4b7fbb86fb0cbba0ac0611af69dc80b0e
Tatsiana Helakhava <Tatsiana_Helakhava@epam.com>
2018-07-07 18:01:03 +0300

    Add common project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
+const store = createStore(reducer, applyMiddleware())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
